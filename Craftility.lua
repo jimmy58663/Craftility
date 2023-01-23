9a9ee4a2bc1291b652a9208d22c00109ea79ce9a
Joshua Chase <jimmy58663@hotmail.com>
2023-01-23 12:28:44 -0700

    Public Order search removed

---
+++ Craftility.lua
@@ -0,0 +1,6 @@
+local addonName, CraftilityNS = ...
+local _G = _G
+local ElvUI = nil
+local Craftility = LibStub("AceAddon-3.0"):NewAddon("Craftility", "AceConsole-3.0", "AceEvent-3.0", "AceTimer-3.0")
+CraftilityNS.Craftility = Craftility
+_G.CraftilityNS = CraftilityNS
