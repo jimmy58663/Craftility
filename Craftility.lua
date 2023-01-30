3107e405a58ef04d3e2bc2b93d2515dc58c36a54
Joshua Chase <jimmy58663@hotmail.com>
2023-01-30 07:55:17 -0700

    Utility function added.

--- Craftility.lua
+++ Craftility.lua
@@ -152,3 +152,23 @@ function Craftility:ParseOrders()
         end
     end
 end
+
+function dumpTable(table, depth)
+    if depth == nil then
+        depth = 0
+    end
+
+    if (depth > 200) then
+        print("Error: Depth > 200 in dumpTable()")
+        return
+    end
+    
+    for k,v in pairs(table) do
+        if (type(v) == "table") then
+            print(string.rep("  ", depth)..k..":")
+            dumpTable(v, depth+1)
+        else
+            print(string.rep("  ", depth)..k..": ",v)
+        end
+    end
+end
